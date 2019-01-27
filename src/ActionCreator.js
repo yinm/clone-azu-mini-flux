bd140e4dc453280a1aa2af15a34f7fb2b37fe258
yinm <yinm.cminor9@gmail.com>
2019-01-27 15:47:56 +0900

    Add ActionCreator

---
+++ src/ActionCreator.js
@@ -0,0 +1,10 @@
+export default class ActionCreator {
+  constructor(dispatcher) {
+    this.dispatcher = dispatcher
+  }
+
+  // "Emit" event ---> Store
+  countUp(data) {
+    this.dispatcher.emit('countUp', data)
+  }
+}
