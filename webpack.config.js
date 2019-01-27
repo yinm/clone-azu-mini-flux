a8e9cce208da337b52381e56fb4a4b0bb6b10202
yinm <yinm.cminor9@gmail.com>
2019-01-27 15:23:54 +0900

    Setup React

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  mode: 'production',
+
+  entry: './src/App.js',
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env',
+                '@babel/react'
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
