61f784892d1637b94001a78ddc30c1000a0cc056
yinm <yinm.cminor9@gmail.com>
2019-01-27 15:38:27 +0900

    Add EventEmitter

---
+++ src/EventEmitter.js
@@ -0,0 +1,31 @@
+export default class EventEmitter {
+  constructor() {
+    this._handlers = {}
+  }
+
+  on(type, handler) {
+    if (typeof this._handlers[type] === 'undefined') {
+      this._handlers[type] = []
+    }
+
+    this._handlers[type].push(handler)
+  }
+
+  emit(type, data) {
+    const handlers = this._handlers[type] || []
+    for (let i = 0; i < handlers.length; i++) {
+      let handler = handlers[i]
+      handler.call(this, data)
+    }
+  }
+
+  off(type, handler) {
+    const handlers = this._handlers[type] || []
+    for (let i = 0; i < handlers.length; i++) {
+      let ownHandler = handlers[i]
+      if (ownHandler === handler) {
+        handlers.splice(i, 1)
+      }
+    }
+  }
+}
